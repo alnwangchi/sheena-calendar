49f3c5a8159647ae9733b890ee8a38d0a96f5479
alnwangchi <alnwangchi@gmail.com>
2024-11-18 00:56:06 +0800

    feat: add paid and send

--- app/home/page.js
+++ app/home/page.js
@@ -1,10 +1,12 @@
 'use client';
 
-import { Card, Col, Row, Statistic, Table } from 'antd';
-import { collection, getDocs } from 'firebase/firestore';
+import { Card, Col, Row, Statistic, Table, message } from 'antd';
+import { collection, doc, getDocs, orderBy, query, updateDoc } from 'firebase/firestore';
 import { ceil, sumBy } from 'lodash-es';
 import { useEffect, useState } from 'react';
 import { db } from '/firebase';
+import dayjs from 'dayjs';
+import { Stamp } from 'lucide-react';
 
 const columns = [
   {
@@ -21,53 +23,119 @@ const columns = [
     title: '門市',
     dataIndex: 'storeId',
     key: 'storeId',
+    render: (storeId) => {
+      return storeId?.replace('門市', '') || '-';
+    },
   },
-  // {
-  //   title: '配送方式',
-  //   dataIndex: 'deliveryMethod',
-  //   key: 'deliveryMethod',
-  // },
   {
-    title: '銀行末五碼',
+    title: '銀行五碼',
     dataIndex: 'bankCode',
     key: 'bankCode',
+  },
+
+  {
+    title: '桌曆',
+    dataIndex: ['calendar', 'quantity'],
+    key: 'calendarQuantity',
+    render: (value, whole) => {
+      const sign = whole?.calendar?.signed;
+      return (
+        <div className='flex gap-2 items-center'>
+          {value} {sign && <Stamp size={16} strokeWidth={1.5} />}
+        </div>
+      );
+    },
+  },
+  {
+    title: '簽名照',
+    dataIndex: ['polaroid', 'quantity'],
+    key: 'polaroidQuantity',
+    render: (value, whole) => {
+      const sign = whole?.polaroid?.signed;
+      return (
+        <div className='flex gap-2 items-center'>
+          {value} {sign && <Stamp size={16} strokeWidth={1.5} />}
+        </div>
+      );
+    },
+  },
+  {
+    title: '下訂時間',
+    dataIndex: 'createdAt',
+    key: 'createdAt',
+    render: (createdAt) => {
+      const displayTime = dayjs(createdAt.seconds * 1000).format('YYYY-MM-DD');
+      return displayTime;
+    },
   },
   {
     title: '總價',
     dataIndex: 'total',
     key: 'total',
+    render: (total) => {
+      return <h3 className='font-bold'>{total}</h3>;
+    },
   },
   {
     title: '是否付款',
     dataIndex: 'havePaid',
     key: 'havePaid',
-    render: (text) => (text ? 'Yes' : 'No'),
-  },
-  {
-    title: '桌曆數量',
-    dataIndex: ['calendar', 'quantity'],
-    key: 'calendarQuantity',
-    render: (value, whole) => {
-      const sign = whole?.calendar?.signed ? ' (需簽)' : '';
-      return `${value} ${sign}`;
+    render: (paid, record) => {
+      return (
+        <button
+          onDoubleClick={async (e) => {
+            e.stopPropagation();
+            try {
+              const docRef = doc(db, 'sheena-202411-calendar', record.id);
+              await updateDoc(docRef, {
+                havePaid: !paid,
+              });
+              message.success('付款狀態已更新');
+              // 重新讀取
+              record.refetch();
+            } catch (error) {
+              console.error('Error updating payment status:', error);
+              message.error('更新失敗');
+            }
+          }}
+          className={`px-4 py-1 rounded whitespace-nowrap ${
+            paid ? 'bg-green-500 text-white' : 'bg-gray-200 text-gray-700'
+          }`}
+        >
+          {paid ? '已付款' : '未付款'}
+        </button>
+      );
     },
   },
   {
-    title: '簽名照數量',
-    dataIndex: ['polaroid', 'quantity'],
-    key: 'polaroidQuantity',
-    render: (value, whole) => {
-      const sign = whole?.calendar?.signed ? ' (需簽)' : '';
-      return `${value} ${sign}`;
-    },
-  },
-  {
-    title: '建立時間',
-    dataIndex: 'createdAt',
-    key: 'createdAt',
-    render: (createdAt) => {
-      const date = new Date(createdAt.seconds * 1000);
-      return date.toLocaleString();
+    title: '是否出貨',
+    dataIndex: 'haveSend',
+    key: 'haveSend',
+    render: (send, record) => {
+      return (
+        <button
+          onDoubleClick={async (e) => {
+            e.stopPropagation();
+            try {
+              const docRef = doc(db, 'sheena-202411-calendar', record.id);
+              await updateDoc(docRef, {
+                haveSend: !send,
+              });
+              message.success('出貨狀態已更新');
+              // 重新讀取
+              record.refetch();
+            } catch (error) {
+              console.error('Error updating payment status:', error);
+              message.error('更新失敗');
+            }
+          }}
+          className={`px-4 py-1 rounded whitespace-nowrap ${
+            send ? 'bg-green-500 text-white' : 'bg-gray-200 text-gray-700'
+          }`}
+        >
+          {send ? '已出貨' : '未出貨'}
+        </button>
+      );
     },
   },
 ];
@@ -79,8 +147,14 @@ const Page = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const querySnapshot = await getDocs(collection(db, 'sheena-202411-calendar'));
-        const dataList = querySnapshot.docs.map((doc) => doc.data());
+        const q = query(collection(db, 'sheena-202411-calendar'), orderBy('createdAt', 'desc'));
+        const querySnapshot = await getDocs(q);
+        const dataList = querySnapshot.docs.map((doc) => ({
+          id: doc.id, // 從 doc 取得 id
+          ...doc.data(), // 展開其他資料
+          refetch: fetchData, // 加入重新讀取函數
+        }));
+
         setData(dataList);
       } catch (error) {
         console.error('Error fetching data: ', error);
